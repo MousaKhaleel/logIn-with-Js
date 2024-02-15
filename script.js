02fe1e03bb73c86ef524f701fa57cf0f63660efc
Mousa Khaleel <mousakhaleel22@gmail.com>
2024-02-15 14:19:57 +0300

    v1.1.0 removed unnecessary lines

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ function consoler(a){
     console.log(a);
 }
 
-// let arr=[];
-// arr[0]=document.getElementById('exampleInputEmail1').value
-// arr[1]=document.getElementById('exampleInputPassword1').value
-// arr[2]=document.getElementById('exampleInputPassword2').value
-// arr[3]=document.getElementById('inlineFormCustomSelect').value
 let str='';
 function buttonClicked(a,b,c,d){
     console.log(a+'\n'+b+'\n'+c+'\n'+d);
